a07f990472fa9e9e06dad3c09f8a05a8cf9d098e
senthilkumar123456 <141622183+senthilkumar123456@users.noreply.github.com>
2023-08-10 09:49:45 +0530

    Add files via upload

---
+++ logical.py
@@ -0,0 +1,17 @@
+##a=int(input('enter the value of a'))
+##b=int(input('enter the value of b'))
+##c=int(input('enter the value of c'))
+##product=a+b+c
+##print('product of three number',product)
+##a=int(input('enter the value of a'))
+##divided=(a/7)
+##print('the value is',divided)
+##a=float(input('enter the munites:'))
+##hour=a/60
+##print('enter the hour',hour)
+##a=float(input('enter ice rate'))
+##b=float(input('enter cram rate'))
+##c=float(input('enter rice rate'))
+##d=float(input('enter milk rate'))
+##totalamount=a+b+c+d
+##print('total amount',totalamount)
